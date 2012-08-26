07ddbe3d514e75f389f263e25d220489291d27d5
Joshua Ruebusch <ruebusjw@muohio.edu>
2012-08-25 23:25:40 -0400

    Added gradual color change and reversal
    several boolean variables added and colors now drain when they reach
    full capacity

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -17,6 +17,12 @@ private:
 	float colorG;
 	float colorB;
 	int selector;
+	bool reverseR;
+	bool reverseG;
+	bool reverseB;
+	bool stopR;
+	bool stopG;
+	bool stopB;
 };
 
 void CatPictureApp::setup()
@@ -25,6 +31,12 @@ void CatPictureApp::setup()
 	colorG = 0.0f;
 	colorB = 0.0f;
 	selector = 0;
+	reverseR = false;
+	reverseG = false;
+	reverseB = false;
+	stopR = false;
+	stopG = false;
+	stopB = false;
 }
 
 void CatPictureApp::mouseDown( MouseEvent event )
@@ -34,18 +46,64 @@ void CatPictureApp::mouseDown( MouseEvent event )
 void CatPictureApp::update()
 {
 	selector = rand() % 3 + 1;
-	if (selector == 1)
+	if (selector == 1 && stopR == false)
 		colorR = colorR + 0.01f;
-	else if (selector == 2)
+	else if (selector == 2 && stopG == false)
 		colorG = colorG + 0.01f;
-	else 
+	else if (selector == 3 && stopB == false) 
 		colorB = colorB +0.01f;
 	if (colorR > 1.0f)
-		colorR = 0.0f;
+	{
+		reverseR = true;
+		stopG = true;
+		stopB = true;
+	}
 	if (colorG > 1.0f)
-		colorG = 0.0f;
+	{
+		stopR = true;
+		reverseG = true;
+		stopB = true;
+	}
 	if (colorB > 1.0f)
-		colorB = 0.0f;
+	{
+		stopR = true;
+		stopG = true;
+		reverseB = true;
+	}
+
+	if (reverseR == true)
+	{
+		colorR = colorR - 0.01f;
+		if (colorR < 0)
+		{
+			reverseR = false;
+			stopG = false;
+			stopB = false;
+		}
+	}
+
+	if (reverseG == true)
+	{
+		colorG = colorG - 0.01f;
+		if (colorG < 0)
+		{
+			stopR = false;
+			reverseG = false;
+			stopB = false;
+		}
+	}
+
+	if (reverseB == true)
+	{
+		colorB = colorB - 0.01f;
+		if (colorB < 0)
+		{
+			stopR = false;
+			stopG = false;
+			reverseB = false;
+		}
+	}
+
 }
 
 void CatPictureApp::draw()
